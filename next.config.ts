ce95ca0a7d524fb81b8c51056539cf2baa45bd35
mahan-teh <mahan.zadeghi@hotmail.com>
2026-03-09 13:19:20 +0100

    fix(ui):git server issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     devIndicators: false,
+    experimental: {
+        serverActions: {
+            allowedOrigins: [
+                "localhost:3000",
+                "*.github.dev",
+                "*.app.github.dev"
+            ]
+        }
+    }
 };
 
 export default nextConfig;
